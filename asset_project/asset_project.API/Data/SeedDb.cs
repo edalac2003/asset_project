288d3636d23645ea0979a54bbf39c21ed4349b7b
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-05-22 15:28:15 -0500

    Fix SeedDb

--- asset_project/asset_project.API/Data/SeedDb.cs
+++ asset_project/asset_project.API/Data/SeedDb.cs
@@ -33,8 +33,8 @@ namespace asset_project.API.Data
                 _context.IdentificationTypes.Add(new IdentificationType { Name = "Cédula de Extranjería", Abbreviation = "CE" });
                 _context.IdentificationTypes.Add(new IdentificationType { Name = "Pasaporte", Abbreviation = "PAS" });
                 _context.IdentificationTypes.Add(new IdentificationType { Name = "Nit", Abbreviation = "NIT" });
+                await _context.SaveChangesAsync();
             }
-            await _context.SaveChangesAsync();
         }
 
         private async Task FillCategories()
@@ -45,8 +45,8 @@ namespace asset_project.API.Data
                 _context.Categories.Add(new Category { Name = "Equipos de Oficina", Description = "Escritorio, Silla, Gabinete, Archivador" });
                 _context.Categories.Add(new Category { Name = "Equipos de Telecomunicaciones", Description = "Rack, Switch, Router, Modem, PBX, Otros" });
                 _context.Categories.Add(new Category { Name = "Electrodomesticos", Description = "Nevera, Estufa, Microondas, Lavadora, Secadora, Lavavajillas, Licuadora" });
+                await _context.SaveChangesAsync();
             }
-            await _context.SaveChangesAsync();
         }
 
         private async Task FillNotificationTypes()
@@ -56,8 +56,8 @@ namespace asset_project.API.Data
                 _context.NotificationTypes.Add(new NotificationType { name = "EMAIL" });
                 _context.NotificationTypes.Add(new NotificationType { name = "SMS" });
                 _context.NotificationTypes.Add(new NotificationType { name = "DASHBOARD" });
+                await _context.SaveChangesAsync();
             }
-            await _context.SaveChangesAsync();
         }
 
         private async Task FillStatusTypes()
@@ -75,35 +75,28 @@ namespace asset_project.API.Data
                 _context.StatusTypes.Add(new StatusType { Name = "Pendiente Insumo", WorkOrder = true });
                 _context.StatusTypes.Add(new StatusType { Name = "Terminada", WorkOrder = true });
                 _context.StatusTypes.Add(new StatusType { Name = "Aprobada", WorkOrder = true });
+                await _context.SaveChangesAsync();
             }
-            await _context.SaveChangesAsync();
         }
 
         private async Task FillCountries()
         {
-            _context.Countries.Add(new Country { Name = "Colombia " });
-
-            await _context.SaveChangesAsync();
-
-            foreach(var country in _context.Countries)
+            if (!_context.Countries.Any())
             {
-                _context.States.Add(new State { Name = "Antioquia", CountryId = country.Id });
+                _context.Countries.Add(new Country { Id = 1, Name = "Colombia " });
+                _context.States.Add(new State { Id = 1, Name = "Antioquia", CountryId = 1 });
+                _context.Cities.Add(new City { Id = 1, Name = "Medellín", StateId = 1 });
+                _context.Cities.Add(new City { Id = 2, Name = "Bello", StateId = 1 });
+                _context.Cities.Add(new City { Id = 3, Name = "Envigado", StateId =1 });
+                _context.Cities.Add(new City { Id = 4, Name = "Itagüí", StateId = 1 });
+                _context.Cities.Add(new City { Id = 5, Name = "Sabaneta", StateId = 1 });
+                _context.Cities.Add(new City { Id = 6, Name = "La Estrella", StateId =1 });
+                _context.Cities.Add(new City { Id = 7, Name = "Caldas", StateId = 1 });
+                _context.Cities.Add(new City { Id = 8, Name = "Copacabana", StateId = 1 });
+                _context.Cities.Add(new City { Id = 9, Name = "Girardota", StateId = 1 });
 
                 await _context.SaveChangesAsync();
-
-                foreach(var state in _context.States)
-                {
-                    _context.Cities.Add(new City { Name = "Medellín", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Bello", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Envigado", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Itagüí", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Sabaneta", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "La Estrella", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Caldas", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Copacabana", StateId = state.Id });
-                    _context.Cities.Add(new City { Name = "Girardota", StateId = state.Id });
-                }
-            }            
+            }
         }
     }
 }
