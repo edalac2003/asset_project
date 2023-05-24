0dd3811ce57114d19db5e8057a2886c4a9f9f11a
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-05-23 23:22:19 -0500

    Create MaintenanceRequestController

---
+++ asset_project/asset_project.Shared/DTO/RequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asset_project.Shared.DTO
+{
+    public class RequestDTO
+    {
+        public string? userName {  get; set; }
+    }
+}
