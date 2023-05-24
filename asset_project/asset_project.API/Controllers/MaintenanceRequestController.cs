504f28c94be4f94eaa5ad7129621830c4fa5ee78
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-05-23 23:22:19 -0500

    Create MaintenanceRequestController

---
+++ asset_project/asset_project.API/Controllers/MaintenanceRequestController.cs
@@ -0,0 +1,31 @@
+﻿using asset_project.API.Data;
+using asset_project.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace asset_project.API.Controllers
+{
+    public class MaintenanceRequestController
+    {
+        private readonly DataContext _context;
+
+        public MaintenanceRequestController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> GetAssets()
+        {
+            return getAssetByUserAsync();
+        }
+
+        private async Task<List<Asset>> getAssetByUserAsync(User user)
+        {
+            var querable = _context.Assets
+                .Where(a => a.Responsible.Equals(user))
+                .AsQueryable();
+
+            return await querable.ToListAsync();    
+        }
+    }
+}
---
+++ asset_project/asset_project.Shared/DTO/ResponseMaintenanceDTO.cs
@@ -0,0 +1,14 @@
+﻿using asset_project.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asset_project.Shared.DTO
+{
+    public class ResponseMaintenanceDTO
+    {
+        public List<Asset>? assets {  get; set; }
+    }
+}
